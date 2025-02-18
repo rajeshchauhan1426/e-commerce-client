2c62080f18ba7ee20248ffdb1d29302b0afdfd89
rajeshchauhan1426 <codedemon1426@gmail.com>
2025-02-18 19:21:02 +0530

    new

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";  
+import Container from "./ui/container";  
+import Link from "next/link";  
+import MainNav from "./main-nav";
 
-function Navbar() {
-  return (
-    <div className='border-b'>Navbar</div>
-  )
-}
+function Navbar() {  
+  return (  
+    <div className="border-b">  
+      <Container>  
+        <div className="relative   px-4 sm:px-4 lg:px-8 flex h-16 items-center -ml-80 ">
+          <Link href="/" className="flex ml-4 lg:ml-0 gap-x-2">  
+            <p className="font-bold text-xl">STORE</p>  
+          </Link>  
+          <MainNav data={[]}/>
+        </div>  
+      </Container>  
+    </div>  
+  );  
+}  
 
-export default Navbar
+export default Navbar;
--- components/ui/container.tsx
+++ components/ui/container.tsx
@@ -1,16 +1,14 @@
-
-import React from "react";
-
-interface ContainerProps{
+interface ContainerProps {
     children: React.ReactNode;
-}
-
-const Container: React.FC<ContainerProps> =({
-    children
-}) => {
-    return(
-        <div className="mx-auto max-w-7xl"> {children}</div>
-    )
-}
-
-export default Container
+  }
+  
+  const Container: React.FC<ContainerProps> = ({ children }) => {
+    return (
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        {children}
+      </div>
+    );
+  };
+  
+  export default Container;
+  
