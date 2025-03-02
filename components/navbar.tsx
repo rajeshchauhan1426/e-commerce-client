027bfaff349b61b1da207b45a2840ee1bddc08cc
rajeshchauhan1426 <codedemon1426@gmail.com>
2025-03-02 20:47:19 +0530

    categories fetch and error solved of navbar not appearing

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,23 +1,26 @@
-import React from "react";  
-import Container from "./ui/container";  
-import Link from "next/link";  
+import Link from "next/link";
+import Container from "./ui/container";
 import MainNav from "./main-nav";
 import getCategories from "@/actions/get-categories";
 
-const Navbar = async ()  => {  
-  const categories = await getCategories()
-  return (  
-    <div className="border-b">  
-      <Container>  
-        <div className="relative   px-4 sm:px-4 lg:px-8 flex h-16 items-center -ml-80 ">
-          <Link href="/" className="flex ml-4 lg:ml-0 gap-x-2">  
-            <p className="font-bold text-xl">STORE</p>  
-          </Link>  
-          <MainNav data={categories}/>
-        </div>  
-      </Container>  
-    </div>  
-  );  
-}  
+const Navbar = async () => {
+  
+    const categories = await getCategories();
+   
+    
+    return (
+      <div className="border-b">
+        <Container>
+          <div className="relative px-4 sm:px-4 lg:px-8 flex h-16 items-center">
+            <Link href="/" className="flex ml-4 lg:ml-0 gap-x-2">
+              <p className="font-bold text-xl">STORE</p>
+            </Link>
+            <MainNav data={categories} />
+          </div>
+        </Container>
+      </div>
+    );
+  } ;
 
-export default Navbar;
+
+export default Navbar
