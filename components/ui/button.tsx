ab093ec9071f7cdf0a230a5e7a38be1500377795
rajeshchauhan1426 <codedemon1426@gmail.com>
2025-03-03 21:20:50 +0530

    button component

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { forwardRef } from "react";
 
 
 export interface ButtonProps
 extends React.ButtonHTMLAttributes<HTMLButtonElement>{}
 
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
+    className,
+    children,
+    disabled,
+    type="button",
+    ...props
+},ref) => {
+    return(
+        <button ref={ref}> {children}</button>
+    )
+})
+
+Button.displayName= "Button"
+
+export default Button
