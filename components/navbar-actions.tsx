1c79b7c8a20a163c78d5e91a04da4ee6660a49df
rajeshchauhan1426 <codedemon1426@gmail.com>
2025-03-03 21:14:38 +0530

    buttons and few components update

---
+++ components/navbar-actions.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NavbarActions() {
+  return (
+    <div className='ml-auto flex items-center gap-x-4'>Navbar Actions</div>
+  )
+}
+
+export default NavbarActions
