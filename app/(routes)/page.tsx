df1affd214288232cfc966e774ad80eebe011ffd
rajeshchauhan1426 <codedemon1426@gmail.com>
2025-03-13 19:50:52 +0530

    billboard ui apperance

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,12 +1,18 @@
+import getBillboard from '@/actions/get-billboard'
 import Billboard from '@/components/billboard'
 import Container from '@/components/ui/container'
 import React from 'react'
 
-function HomePage() {
+export const revalidate = 0;
+
+const HomePage = async() => {
+  const billboard = await getBillboard("549785e3-5edb-47da-835f-88df9518ef63");
+
+
   return (
    <Container>
      <div className='space-y-10 pb-10'>
-      <Billboard/>
+      <Billboard data={billboard}/>
      </div>
 
    </Container>
