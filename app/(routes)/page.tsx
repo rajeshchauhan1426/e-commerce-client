c502e364f2a4b1dfa83fe1ef783e0fecaa28494c
rajeshchauhan1426 <codedemon1426@gmail.com>
2025-03-21 20:56:49 +0530

    ui

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -17,9 +17,10 @@ const HomePage = async() => {
    <Container>
      <div className='space-y-10 pb-10'>
       <Billboard data={billboard}/>
-     </div>
+     
      <div className='flex flex-colgap-y-8 px-4 sm:px-6 lg:px-8'>
         <ProductList title="Featured Product" items={products}/>
+     </div>
      </div>
 
    </Container>
