309ff2801fc4453e2ba7a9d48816111688448f4c
rajeshchauhan1426 <codedemon1426@gmail.com>
2025-03-26 20:56:02 +0530

    few error are yet to be solved

--- components/gallery/gallerytab.tsx
+++ components/gallery/gallerytab.tsx
@@ -1,35 +1,33 @@
 import React from 'react'
+import { Image as ImageType } from '@/types'
+import { Tab } from '@headlessui/react'
+import Image from 'next/image'
+import { cn } from '@/libs/libs'
 
-
-import { Image as ImageType } from '@/types';
-import { Tab } from '@headlessui/react';
-import Image from 'next/image';
-import { cn } from '@/libs/libs';
-
-interface GalleryTabProps{
-    images: ImageType
+interface GalleryTabProps {
+  image: ImageType
 }
 
-const GalleryTab: React.FC<GalleryTabProps> =({
-    images
-}) => {
+const GalleryTab: React.FC<GalleryTabProps> = ({ image }) => {
   return (
-   <Tab className='relative flex aspect-square cursor-pointer items-center justify-center rounded-md bg-white'>{({selected}) => (
-    <div>
-        <span className='absolute h-full w-full aspect-square inset-0 overflow-hidden rounded-md'>
-            <Image
+    <Tab className="relative aspect-square cursor-pointer rounded-md bg-white overflow-hidden">
+      {({ selected }) => (
+        <>
+          <Image
             fill
-            src={images.url}
-            alt=''
-            className='object-cover object-center'
-            />
-
-        </span>
-        <span className={cn(
-            "absolute inset-0 rounded-md ring-2 ring-offset-2", selected ? "ring-black" : "ring-transparent"
-        )}/>
-    </div>
-   )}</Tab>
+            src={image.url}
+            alt=""
+            className="object-cover object-center"
+          />
+          <span
+            className={cn(
+              "absolute inset-0 rounded-md ring-2 ring-offset-2",
+              selected ? "ring-black" : "ring-transparent"
+            )}
+          />
+        </>
+      )}
+    </Tab>
   )
 }
 
