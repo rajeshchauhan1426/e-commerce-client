f513534b5b649c2f31770fc9840106ed52c8e36f
rajeshchauhan1426 <codedemon1426@gmail.com>
2025-05-04 22:03:18 +0530

    u

--- components/info.tsx
+++ components/info.tsx
@@ -1,6 +1,9 @@
+"use client"
 import { Product } from "@/types";
 import React from "react";
 import Currency from "./ui/currency";
+import Button from "./ui/button";
+import { ShoppingCart } from "lucide-react";
 
 interface InfoProps{
     data: Product;
@@ -18,6 +21,7 @@ const Info: React.FC<InfoProps> =({
                 </p>
             </div>
             <hr  className="my-4"/>
+            <div className="flex flex-col gap-y-6" >
             <div className=" flex items-center gap-x-4">
                 <h3 className="font-semibold text-black">Size:</h3>
                 <div>
@@ -28,6 +32,12 @@ const Info: React.FC<InfoProps> =({
                 <h3 className="font-semibold text-black">Colour:</h3>
                 <div className="h-6 w-6 rounded-full border border-gray-600" style={{backgroundColor: data?.color?.value}}></div>
             </div>
+        </div>
+        <div className="mt-10 flex items-center gap-x-3">
+            <Button className="flex items-center gap-x-2">Add To Cart  
+                <ShoppingCart/>
+            </Button>
+        </div>
         </div>
     )
 }
